86942958961b4e5e97372aac21872879cec8f994
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-03-08 14:20:02 +0530

    added modified BMI Calculations

--- app/src/main/java/com/waudeshindumina/uicalculator/BMICalculation.kt
+++ app/src/main/java/com/waudeshindumina/uicalculator/BMICalculation.kt
@@ -14,20 +14,13 @@ private fun Nothing?.setOnClickListener(function: () -> Unit) {
 }
 
 class BMICalculation : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmicalculation)
-
-        var button_Calculator = findViewById<Button>(R.id.button_Calculator)
-        button_Calculator.setOnClickListener {
-            val intent2 = Intent(this, MainActivity::class.java)
-            startActivity(intent2)
-        }
-
         val btnCalculate = null
           btnCalculate.setOnClickListener {
-
-            if (!edtWeight.text.toString().equals("") && !edtHeightFt.text.toString().equals("") && !edtHeightIn.text.toString().equals("")){
+              if (!edtWeight.text.toString().equals("") && !edtHeightFt.text.toString().equals("") && !edtHeightIn.text.toString().equals("")){
                 val wt = (edtWeight.text.toString().toDouble())
                 val htFeet = (edtHeightFt.text.toString().toDouble())
                 val htInch = (edtHeightIn.text.toString().toDouble())
@@ -42,12 +35,14 @@ class BMICalculation : AppCompatActivity() {
                 if (bmi>25){
                     Toast.makeText(this@BMICalculation,"You're OverWeight",Toast.LENGTH_LONG).show();
                 }
-
-
-        }else{
-            Toast.makeText(this@BMICalculation,"Please fill ip all the required ",Toast.LENGTH_LONG).show();
+              }else{
+                Toast.makeText(this@BMICalculation,"Please fill ip all the required ",Toast.LENGTH_LONG).show();
+              }
+          }
+        var button_Calculator = findViewById<Button>(R.id.button_Calculator)
+            button_Calculator.setOnClickListener {
+                val intent2 = Intent(this, MainActivity::class.java)
+                startActivity(intent2)
             }
-            }
-
     }
 }
